cc2f1aaffea33d541962b2608f93004d79df3565
Thomas Edwin Santosa <thomas@katalisindonesia.com>
2022-12-17 22:09:58 +0700

    Add 0 values to undetected types

--- src/main/kotlin/com/katalisindonesia/banyuwangi/repo/CameraRepo.kt
+++ src/main/kotlin/com/katalisindonesia/banyuwangi/repo/CameraRepo.kt
@@ -4,13 +4,14 @@ import com.katalisindonesia.banyuwangi.model.Camera
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.Optional
 import java.util.UUID
 
 @Repository
-interface CameraRepo : JpaRepository<Camera, UUID> {
+interface CameraRepo : JpaRepository<Camera, UUID>, JpaSpecificationExecutor<Camera> {
     fun getCameraByVmsCameraIndexCode(vmsCameraIndexCode: String): Optional<Camera>
 
     @Query("select t from Camera t where t.isActive = :active")
