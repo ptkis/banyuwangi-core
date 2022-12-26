5c22d155744f1dbcfde68fc9c6d8eeacc133aa63
Thomas Edwin Santosa <thomas@katalisindonesia.com>
2022-12-27 01:20:35 +0700

    typo

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -5,4 +5,4 @@ export
 [[ -z "$YOURKIT_BROKER_URL" ]] && { echo "Please set YOURKIT_BROKER_URL" ; exit 1; }
 [[ -z "$YOURKIT_BROKER_TOKEN" ]] && { echo "Please set YOURKIT_BROKER_TOKEN" ; exit 1; }
 
-exec java -agentpath:/usr/local/YourKit-JavaProfiler-2022.9/bin/linux-x86-64/libyjpagent.so=broker_url=$YOURKIT_BROKER_URL,broker_token=$YOURKIT_YOURKIT_BROKER_TOKEN $JAVA_OPTS -jar "$ARTIFACT-$VERSION.jar" $@
+exec java -agentpath:/usr/local/YourKit-JavaProfiler-2022.9/bin/linux-x86-64/libyjpagent.so=broker_url=$YOURKIT_BROKER_URL,broker_token=$YOURKIT_BROKER_TOKEN $JAVA_OPTS -jar "$ARTIFACT-$VERSION.jar" $@
