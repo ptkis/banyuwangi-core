b7a94aec69d68867821e79a65fbb46d59f382ca1
Thomas Edwin Santosa <thomas@katalisindonesia.com>
2024-03-28 11:39:23 +0700

    Disable oauth2-client from gradle and disable token manager

--- src/main/kotlin/com/katalisindonesia/banyuwangi/security/TokenManager.kt
+++ src/main/kotlin/com/katalisindonesia/banyuwangi/security/TokenManager.kt
@@ -1,16 +1,14 @@
 package com.katalisindonesia.banyuwangi.security
 
-import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
-
 // @Component
-class TokenManager(private val authorizedClientManager: OAuth2AuthorizedClientManager) {
+class TokenManager/*(private val authorizedClientManager: OAuth2AuthorizedClientManager)*/ {
     fun accessToken(clientRegistrationName: String, clientId: String): String {
-        val authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationName)
+/*        val authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationName)
             .principal(clientId)
             .build()
         val authorizedClient = authorizedClientManager.authorize(authorizeRequest)
         val accessToken = authorizedClient!!.accessToken
-        return accessToken.tokenValue
+        return accessToken.tokenValue*/
+        return clientRegistrationName + clientId // dummy
     }
 }
