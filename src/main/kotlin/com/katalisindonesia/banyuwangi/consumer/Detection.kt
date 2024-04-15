e91a56622675a107f98e27b6852801157d9ad289
Thomas Edwin Santosa <thomas@katalisindonesia.com>
2024-04-15 15:31:12 +0700

    Ignore unknown properties in detection

--- src/main/kotlin/com/katalisindonesia/banyuwangi/consumer/Detection.kt
+++ src/main/kotlin/com/katalisindonesia/banyuwangi/consumer/Detection.kt
@@ -1,5 +1,8 @@
 package com.katalisindonesia.banyuwangi.consumer
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Detection(
     var boundingBox: BoundingBox? = BoundingBox(),
     var className: String? = null,
